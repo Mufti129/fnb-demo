5bbb145c19dedbd25b2430aad3ff913651d6d140
Mufti129 <ukhtytrader01@gmail.com>
2026-03-31 20:42:16 +0700

    overview.py

--- dashboard/overview.py
+++ dashboard/overview.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import pandas as pd
 from modules.sales.sales_service import get_daily_sales
 
-def show_overview():
+def show_overview(start_date=None, end_date=None):
     st.subheader("📈 Penjualan Harian")
 
-    df = get_daily_sales()
-    st.line_chart(df.set_index("date"))
+    df = get_daily_sales(start_date, end_date)
+
+    if not df.empty:
+        df["date"] = pd.to_datetime(df["date"])
+        st.line_chart(df.set_index("date"))
