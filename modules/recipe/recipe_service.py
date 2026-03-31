f76d2e46638a5ec2d7aac763210626f462863e6c
Mufti129 <ukhtytrader01@gmail.com>
2026-03-31 20:53:23 +0700

    recipe_service.py

--- modules/recipe/recipe_service.py
+++ modules/recipe/recipe_service.py
@@ -1,5 +1,6 @@
-from utils.gsheet_loader import load_sheet
+bfrom utils.gsheet_loader import load_sheet
 import pandas as pd
+
 
 def calculate_usage(start_date=None, end_date=None):
     sales = load_sheet("sales")
@@ -19,3 +20,23 @@ def calculate_usage(start_date=None, end_date=None):
     merged["total_usage"] = merged["qty"] * merged["ingredient_qty"]
 
     return merged.groupby(["ingredient_id","unit"])["total_usage"].sum().reset_index()
+
+
+def usage_per_product(start_date=None, end_date=None):
+    sales = load_sheet("sales")
+    recipe = load_sheet("recipe")
+
+    sales["date"] = pd.to_datetime(sales["date"])
+
+    if start_date:
+        start_date = pd.to_datetime(start_date)
+        sales = sales[sales["date"] >= start_date]
+
+    if end_date:
+        end_date = pd.to_datetime(end_date)
+        sales = sales[sales["date"] <= end_date]
+
+    merged = sales.merge(recipe, on="product_id")
+    merged["usage"] = merged["qty"] * merged["ingredient_qty"]
+
+    return merged.groupby(["product_id","ingredient_id"])["usage"].sum().reset_index()
