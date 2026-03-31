dc9c8ea645495bf5a141788024f6211f6ae4a6f0
Mufti129 <ukhtytrader01@gmail.com>
2026-03-31 20:43:30 +0700

    sales_product.py

--- dashboard/sales_product.py
+++ dashboard/sales_product.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from modules.sales.sales_service import get_sales_per_product
 
-def show_sales_product():
+def show_sales_product(start_date=None, end_date=None):
     st.subheader("📊 Penjualan per Produk")
 
-    df = get_sales_per_product()
-
+    df = get_sales_per_product(start_date, end_date)
     df["total"] = df["total"].apply(lambda x: f"Rp {x:,.0f}")
 
     st.dataframe(df)
