ba650692e6ff41f86afd3f7e1128aa8496814720
Mufti129 <ukhtytrader01@gmail.com>
2026-03-31 20:53:56 +0700

    sales_service.py

--- modules/sales/sales_service.py
+++ modules/sales/sales_service.py
@@ -1,7 +1,6 @@
 from utils.gsheet_loader import load_sheet
 import pandas as pd
 
-import pandas as pd
 
 def get_daily_sales(start_date=None, end_date=None):
     df = load_sheet("sales")
@@ -22,6 +21,12 @@ def get_daily_sales(start_date=None, end_date=None):
 def get_sales_per_product(start_date=None, end_date=None):
     df = load_sheet("sales")
     df["date"] = pd.to_datetime(df["date"])
+
+    if start_date:
+        start_date = pd.to_datetime(start_date)
+
+    if end_date:
+        end_date = pd.to_datetime(end_date)
 
     if start_date and end_date:
         df = df[(df["date"] >= start_date) & (df["date"] <= end_date)]
