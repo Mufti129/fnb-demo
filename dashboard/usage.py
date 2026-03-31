1f0847f0ee6e8e5398161a7d0c7a14654879d29a
Mufti129 <ukhtytrader01@gmail.com>
2026-03-31 20:43:06 +0700

    usage.py

--- dashboard/usage.py
+++ dashboard/usage.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from modules.recipe.recipe_service import calculate_usage
 
-def show_usage():
+def show_usage(start_date=None, end_date=None):
     st.subheader("🍳 Pemakaian Bahan")
 
-    df = calculate_usage()
+    df = calculate_usage(start_date, end_date)
     df["total_usage"] = df["total_usage"].astype(str) + " " + df["unit"]
 
     st.dataframe(df)
