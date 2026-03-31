1ba9bf54c0edcdd01d7cf3dd7643ce38895ddeb2
Mufti129 <ukhtytrader01@gmail.com>
2026-03-31 20:43:53 +0700

    usage_product.py

--- dashboard/usage_product.py
+++ dashboard/usage_product.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from modules.recipe.recipe_service import usage_per_product
 
-def show_usage_product():
-    st.subheader("🍳 Pemakaian Bahan per Produk")
+def show_usage_product(start_date=None, end_date=None):
+    st.subheader("🍳 Pemakaian per Produk")
 
-    df = usage_per_product()
+    df = usage_per_product(start_date, end_date)
 
     st.dataframe(df)
