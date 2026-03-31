de1cd16d9877078df2ac81f95898b8fa4e07e56f
Mufti129 <ukhtytrader01@gmail.com>
2026-03-31 20:41:15 +0700

    recipe_service.py

--- modules/recipe/recipe_service.py
+++ modules/recipe/recipe_service.py
@@ -1,22 +1,37 @@
 from utils.gsheet_loader import load_sheet
+import pandas as pd
 
-def calculate_usage():
+def calculate_usage(start_date=None, end_date=None):
     sales = load_sheet("sales")
     recipe = load_sheet("recipe")
 
-    merged = sales.merge(recipe, on="product_id")
+    sales["date"] = pd.to_datetime(sales["date"])
 
+    if start_date:
+        sales = sales[sales["date"] >= start_date]
+
+    if end_date:
+        sales = sales[sales["date"] <= end_date]
+
+    merged = sales.merge(recipe, on="product_id")
     merged["total_usage"] = merged["qty"] * merged["ingredient_qty"]
 
     return merged.groupby(["ingredient_id","unit"])["total_usage"].sum().reset_index()
 
 
-def usage_per_product():
+def usage_per_product(start_date=None, end_date=None):
     sales = load_sheet("sales")
     recipe = load_sheet("recipe")
 
-    merged = sales.merge(recipe, on="product_id")
+    sales["date"] = pd.to_datetime(sales["date"])
 
+    if start_date:
+        sales = sales[sales["date"] >= start_date]
+
+    if end_date:
+        sales = sales[sales["date"] <= end_date]
+
+    merged = sales.merge(recipe, on="product_id")
     merged["usage"] = merged["qty"] * merged["ingredient_qty"]
 
     return merged.groupby(["product_id","ingredient_id"])["usage"].sum().reset_index()
