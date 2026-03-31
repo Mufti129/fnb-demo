53e785b163d7d78f1e2ce3a20289444bb79300c7
Mufti129 <ukhtytrader01@gmail.com>
2026-03-31 19:40:43 +0700

    gsheet_loader.py

--- utils/gsheet_loader.py
+++ utils/gsheet_loader.py
@@ -2,9 +2,15 @@ import pandas as pd
 import streamlit as st
 from config.gsheet_config import connect_gsheet
 
+SPREADSHEET_ID = "13wQcCXIYRb__HO-Vb0Zm_f77u9628uKt4YDgD94IpiY"
+
 @st.cache_data
 def load_sheet(sheet_name):
     client = connect_gsheet()
-    sheet = client.open("FNB_DATABASE").worksheet(sheet_name)
+
+    spreadsheet = client.open_by_key(SPREADSHEET_ID)
+    sheet = spreadsheet.worksheet(sheet_name)
+
     data = sheet.get_all_records()
+
     return pd.DataFrame(data)
